494ae31c27dddae91e06a69f082257541a35b0a2
Vivixing <echeverri.vivian1120@gmail.com>
2024-11-23 20:24:22 -0500

    Agregada lógica en el ts de subasta

--- src/app/pages/auction/auction.page.ts
+++ src/app/pages/auction/auction.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
+import { AuctionService } from 'src/app/services/auction.service';
 
 
 @Component({
@@ -9,9 +10,42 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class AuctionPage implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  scannedCharacters: any[] = [];
+  desiredCharacters: any[] = [];
+  selectedScannedCharacter : any;
+  selectedDesiredCharacter : any;
+
+  constructor(private authService: AuthService, private auctionService: AuctionService) { }
 
   ngOnInit() {
+  }
+
+  async loadAuction(){
+    try{
+      this.scannedCharacters = await this.auctionService.getAuctionsByCreator(this.authService.idUserLogged());
+      this.desiredCharacters = await this.auctionService.getAvailableAuctions();
+    }catch (error){
+      console.error('Error loading auctions', error);
+    }
+  }
+
+  async proposeExchange(){
+    if(this.selectedScannedCharacter && this.selectedDesiredCharacter){
+      try{
+        const auctionData = {
+          character1Id : this.selectedScannedCharacter.id,
+          character2Id : this.selectedDesiredCharacter.id,
+          auctionCreator : this.authService.idUserLogged()
+        };
+        const response = await this.auctionService.exchangeCharacters(auctionData,this.authService.idUserLogged() ); 
+        console.log('Exchange proposed successfully:', response);
+        await this.loadAuction();
+      }catch (error) {
+        console.error('Error proposing exchange', error);
+      }
+    } else{
+      alert('Selecciona ambos personajes para el intercambio.');
+    }
   }
 
   logOut() {
